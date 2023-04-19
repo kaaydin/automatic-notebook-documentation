d4e3bc48c38e7e2de35ec5559ee6ecd237ddc10f
kaaydin <kaanaydin95@icloud.com>
2023-04-19 23:13:10 +0200

    Updated files

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import nbconvert
-
 from main import main
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
+st.set_page_config(layout="wide", page_title="Automatic Documentation for Jupyter Notebooks")
 
-st.write("## Remove background from your image")
+st.write("## Automatically generate documentation for your Jupyter Notebook")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":computer: Try uploading a Jupyter Notebook to watch how documentation is added magically. The adjusted notebook can be downloaded from the sidebar"
 )
 st.sidebar.write("## Upload and download :gear:")
 
-
-# Download the fixed image
-
-def render_notebook(notebook_path):
-    with open(notebook_path, "r") as nb_file:
-        nb_content = nb_file.read()
-
+def render_notebook(notebook_content):
     html_exporter = nbconvert.HTMLExporter()
-    notebook_html, _ = html_exporter.from_notebook_node(nbconvert.reads(nb_content, as_version=4))
+    notebook_html, _ = html_exporter.from_notebook_node(nbconvert.reads(notebook_content, as_version=4))
     return notebook_html
 
 def generate_new_notebook(upload):
-    notebook = upload
+    notebook = upload.read()
     col1.write("Original Notebook :camera:")
-    col1.render_notebook(notebook)
+    col1_html = render_notebook(notebook)
+    components.html(col1_html, height=800)
 
-    adjusted_notebook = main(upload)
-    col2.write("Fixed Image :wrench:")
-    col2.render_notebook(adjusted_notebook)
+    adjusted_notebook = main(notebook)
+    col2.write("Documented Notebook :wrench:")
+    col2_html = render_notebook(adjusted_notebook)
+    components.html(col2_html, height=800)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", adjusted_notebook, "adjusted_notebook.ipynb", "image/png")
-
+    st.sidebar.download_button("Download documented notebook", adjusted_notebook, "documented_notebook.ipynb", "application/x-ipynb+json")
 
 col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["ipynb"])
+my_upload = st.sidebar.file_uploader("Upload a notebook", type=["ipynb"])
+
+if my_upload:
+    generate_new_notebook(my_upload)
