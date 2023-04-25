e54c207c6161af721df8a167cfcc95e541b7ac64
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-04-25 11:35:33 +0200

    M

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def generate_new_notebook(upload):
     st.write(notebook_dict.keys())
     st.write("And the cells")
     st.write(notebook_dict["cells"][0]["source"])
+    st.write("Furthermore")
+    st.write(notebook_dict["cells"][0]["source"])
     
     messages = create_messagelist(notebook)
     nb = nbformat.v4.new_notebook()
