5d77f2d341da018d084f53b47c6fc00fc3253a8e
kaaydin <kaanaydin95@icloud.com>
2023-04-26 19:52:41 +0200

    Updated files

--- app.py
+++ app.py
@@ -1,32 +1,51 @@
+
+
+## Importing all relevant functions
 import streamlit as st
-import streamlit.components.v1 as components
 import nbformat
 import json
-from IPython.core.display import HTML
 from nbconvert import HTMLExporter
 
 from comment_generator import query_message_list
 from utils import create_messagelist
 
+## Setting website configurations
 st.set_page_config(layout="wide", page_title="Automatic Documentation for Jupyter Notebooks")
+col1, col2 = st.columns(2)
 
+## Writing title on website
 st.write("## Automatically generate documentation for your Jupyter Notebook")
+
+## Writing description text on website
 st.write(":computer: Try uploading a Jupyter Notebook to watch how documentation is added magically. The adjusted notebook can be downloaded from the sidebar")
+
+## Setting download sidebar
 st.sidebar.write("## Upload and download :gear:")
+my_upload = st.sidebar.file_uploader("Upload a notebook", type=["ipynb"])
+
+
+## Function to define 
 
 def generate_new_notebook(upload):
+    
+    ## Reading notebook as OTF
     notebook = upload.read().decode("utf-8")
 
-    col1.write("Original Notebook :camera:")
-    # col1_html = notebook
-    # components.html(col1_html, height=800)
+    ## Writing title for the first column 
+    col1.write("Original notebook :camera:")
+    
+    ## Writing title for the second column
+    col2.write("Updated notebook :camera:")
+
+    #col1_html = notebook
+    #components.html(col1_html, height=800)
+
+    
     
     ## At this point, the notebook element is a string. We can try to turn it into a dictionary:
     notebook_dict = json.loads(notebook)
     
-    ## Access all code snippets and ########This is the format for accessing all of the code snippets. We can change the index in the second one to access the different
-    ######## snippets based on their index:notebook_dict["cells"][0]["source"]
-    ## And then pass this dictionairy to the create_messagelist. This will allows us to access all code cells
+
     
     messages = create_messagelist(notebook_dict)
 
@@ -46,18 +65,23 @@ def generate_new_notebook(upload):
     col2.write("The notebook code")
     nb_true_quotes = json.dumps(nb, indent = 4) 
 
-    
-    st.write(nb_true_quotes)
+
     nb_encoded = str(nb_true_quotes).encode('utf-8')
     st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
 
-    html_exporter = HTMLExporter()
-    notebook_html, _ = html_exporter.from_notebook_node(nb_encoded)
-    col2.write(HTML(notebook_html), unsafe_allow_html=True)
 
-col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload a notebook", type=["ipynb"])
 
 if my_upload:
     generate_new_notebook(my_upload)
+else:
+    st.write("Please upload a Jupyter Notebook to view.")
 
+
+
+# col1_html = notebook
+    # components.html(col1_html, height=800)
+
+
+        ## Access all code snippets and ########This is the format for accessing all of the code snippets. We can change the index in the second one to access the different
+    ######## snippets based on their index:notebook_dict["cells"][0]["source"]
+    ## And then pass this dictionairy to the create_messagelist. This will allows us to access all code cells
