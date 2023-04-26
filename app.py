adfe6fd189fea3537537e8a04ee3c1099789bc90
kaaydin <kaanaydin95@icloud.com>
2023-04-26 19:28:46 +0200

    Updated Files

--- app.py
+++ app.py
@@ -1,85 +1,69 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import nbconvert
 import nbformat
 import json
-import re
+from IPython.core.display import HTML
+from streamlit_echarts import st_echarts
+from nbconvert import HTMLExporter
 
-from comment_generator import query_message_list, run_api
-from utils import read_notebook, create_notebook, save_notebook, write_notebook, create_messagelist, correct_spacing
+from comment_generator import query_message_list
+from utils import create_messagelist
 
 st.set_page_config(layout="wide", page_title="Automatic Documentation for Jupyter Notebooks")
 
 st.write("## Automatically generate documentation for your Jupyter Notebook")
-st.write(
-    ":computer: Try uploading a Jupyter Notebook to watch how documentation is added magically. The adjusted notebook can be downloaded from the sidebar"
-)
+st.write(":computer: Try uploading a Jupyter Notebook to watch how documentation is added magically. The adjusted notebook can be downloaded from the sidebar")
 st.sidebar.write("## Upload and download :gear:")
 
 def generate_new_notebook(upload):
     notebook = upload.read().decode("utf-8")
-    
+
     col1.write("Original Notebook :camera:")
     col1_html = notebook
     components.html(col1_html, height=800)
-    #At this point, the notebook element is a string. We can try to turn it into a dictionairy:
+    
+    ## At this point, the notebook element is a string. We can try to turn it into a dictionairy:
     notebook_dict = json.loads(notebook)
-    ########This is the format for accessing all of the code snippets. We can change the index in the second one to access the different
+    
+    ## Access all code snippets and ########This is the format for accessing all of the code snippets. We can change the index in the second one to access the different
     ######## snippets based on their index:notebook_dict["cells"][0]["source"]
-    #And then pass this dictionairy to the create_messagelist. This will allows us to access all code cells
+    ## And then pass this dictionairy to the create_messagelist. This will allows us to access all code cells
+    
     messages = create_messagelist(notebook_dict)
 
-    # st.write("The format of the messages")
-    # st.write(messages)
-    ###################Call to GPT-3.5 here: 
-
+    ## Query call to GPT-3.5
     GPT_return = query_message_list(messages)
-    #######################
-
-
+    
+    ## Instantiate new notebook
     nb = nbformat.v4.new_notebook()
     
+    ## Add messages from GPT query to new notebook
     for message in GPT_return:
-
-        new_cell = nbformat.v4.new_code_cell(message) # old: new_cell = nbformat.v4.new_code_cell(message)
+        new_cell = nbformat.v4.new_code_cell(message)
         nb.cells.append(new_cell)
 
-    # st.write("The new Notebook!")
-    # st.write(nb)
-    # st.write("The encoded version")
-    # st.write(str(nb))
+    ## 
+
     st.write("The notebook code")
-    # for i in range(len(nb["cells"])):
-    #     # nb["cells"][i]["source"] =  nb["cells"][i]["source"].replace("'", '"')
-    #     # nb["cells"][i]["source"] =  nb["cells"][i]["source"].replace('"', '\\"')
-
-    #     # nb["cells"][i]["source"] = json.dumps(nb["cells"][i]["source"])
-    #     st.write(nb["cells"][i]["source"])
-
-    
-    # nb_true_quotes = str(nb).replace("'", '"')
-    # nb_true_quotes = nb_true_quotes.replace("None", "null")
-
     nb_true_quotes = json.dumps(nb, indent = 4) 
 
     
     st.write(nb_true_quotes)
-    # nb_true_quotes = nb_true_quotes.replace('"training"', '\\"training\\"')
-    # st.write(nb_true_quotes)
     nb_encoded = str(nb_true_quotes).encode('utf-8')
-    # st.write("New encoded version")
-    # st.write(nb_encoded)
-    # st.write("After change")
-    # st.write(str(nb_true_quotes))
-    # col2.write("Documented Notebook :wrench:")
-    # col2_html = render_notebook(adjusted_notebook)
-    # components.html(col2_html, height=800)
-    # st.sidebar.markdown("\n")
     st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
+
+def display_notebook(notebook_data):
+    html_exporter = HTMLExporter()
+    notebook_html, _ = html_exporter.from_notebook_node(notebook_data)
+    st.write(HTML(notebook_html), unsafe_allow_html=True)
     
+
 
 col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("Upload a notebook", type=["ipynb"])
 
 if my_upload:
+    notebook_data = json.load(my_upload)
+    display_notebook(notebook_data)
     generate_new_notebook(my_upload)
+
