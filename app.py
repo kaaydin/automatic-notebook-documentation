d3aa4298c468a4998daa61bfd8ec6f1540b986f1
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-04-26 14:38:38 +0200

    M

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def generate_new_notebook(upload):
     # st.write("The encoded version")
     # st.write(str(nb))
     st.write("The notebook code")
-    st.write(nb.keys())
+    st.write(nb["cells"])
 
     nb_true_quotes = str(nb).replace("'", '"')
     nb_true_quotes = nb_true_quotes.replace("None", "null")
