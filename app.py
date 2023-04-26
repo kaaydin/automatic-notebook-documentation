8de7e7554ecf488e105afd277eb87f6a268590e4
kaaydin <kaanaydin95@icloud.com>
2023-04-26 23:26:41 +0200

    Updated files

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def generate_new_notebook(upload):
         #relevant = nb_true_quotes["cells"][k]["source"]
         #col2.code(relevant)
 
-    col2.write(GPT_return)
+    col2.write(nb)
     
     ## Creating download button with the updated notebook
     st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
