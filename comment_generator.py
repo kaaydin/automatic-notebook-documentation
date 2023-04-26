0ea8e7ceb7843fe8fb3d13f0919d6d1911576bce
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-04-26 11:15:32 +0200

    M

--- comment_generator.py
+++ comment_generator.py
@@ -29,8 +29,8 @@ def query_message_list(messages):
 
     for message in tqdm(messages):
       output = run_api(chosen_model = MODEL, instruction = INSTRUCTION, message = message, temperature=TEMPERATURE)
-      output = output.replace("'", '"')
-      output = output.replace('"', '\"') #New line
+      #output = output.replace('"', '"')
+      #output = output.replace('"', '\\"') #New line
       outputs.append(output)
 
     return outputs
