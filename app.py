bde43952ff8177e060e2c9d0255c33d315b661c3
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-04-26 13:57:09 +0200

    M

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def generate_new_notebook(upload):
     
     for message in GPT_return:
         st.write(message)
+        message = message.replace("'", '\"')
         message = message.replace('"', '\"')
         new_cell = nbformat.v4.new_code_cell(message) # old: new_cell = nbformat.v4.new_code_cell(message)
         nb.cells.append(new_cell)
