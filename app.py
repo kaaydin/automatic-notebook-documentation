bd484436290a8455633e9d2841a1649d4f6d6d92
kaaydin <kaanaydin95@icloud.com>
2023-04-29 13:33:08 +0200

    Updating files

--- app.py
+++ app.py
@@ -70,11 +70,10 @@ def generate_new_notebook(upload):
 
     html_exporter = HTMLExporter()
     (html_output, _) = html_exporter.from_notebook_node(nb)
-    st_html(html_output, width=900, height=800, scrolling=True)
+    col2.write(st_html(html_output, width=900, height=800, scrolling=True))
 
     ## Creating download button with the updated notebook
     st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
-
 
 
     ## Second visualisation
@@ -94,16 +93,16 @@ def generate_new_notebook(upload):
     #nb_html, _ = html_exporter.from_notebook_node(nb_json)
     #with st.beta_expander("Notebook content"):
         #st.write(nb_html, unsafe_allow_html=True)
+    
 
-    ## Creating download button with the updated notebook
-    st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
-    notebook_edited = download.read().decode("utf-8")
-    notebook_dict = json.loads(notebook_edited)
-    messages = create_messagelist(notebook_dict)
-    for message in messages:
-        notebook_visualisation = message["content"]
-        modified_string = notebook_visualisation.replace("\n ", "\n")
-        col2.code(modified_string)
+
+    #notebook_edited = download.read().decode("utf-8")
+    #notebook_dict = json.loads(notebook_edited)
+    #messages = create_messagelist(notebook_dict)
+    #for message in messages:
+    #    notebook_visualisation = message["content"]
+    #    modified_string = notebook_visualisation.replace("\n ", "\n")
+    #    col2.code(modified_string)
 
 
 
