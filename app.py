cf7450ad19940e758c4c99f9dda98c24e2dc02c4
kaaydin <kaanaydin95@icloud.com>
2023-04-29 11:43:35 +0200

    Updating files

--- app.py
+++ app.py
@@ -64,11 +64,11 @@ def generate_new_notebook(upload):
     nb_encoded = str(nb_true_quotes).encode('utf-8')
 
     ## Second visualisation
-    #notebook_dict_edited = type(nb_true_quotes)
-    #col2.write(notebook_dict_edited)
-    col2.write(type(nb_true_quotes))
+    test_text = nb_encoded["cells"]
+    col2.write(test_text)
+    #col2.write(type(nb_true_quotes))
     #notebook_dict_edited2 = type(nb_encoded)
-    col2.write(type(nb_encoded))
+    #col2.write(type(nb_encoded))
     
     ## Creating download button with the updated notebook
     st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
