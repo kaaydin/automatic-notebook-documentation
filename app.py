aa1515b30181118f2e6fdbb3e317275620906679
kaaydin <kaanaydin95@icloud.com>
2023-04-29 13:36:00 +0200

    Updating files

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def generate_new_notebook(upload):
 
     html_exporter = HTMLExporter()
     (html_output, _) = html_exporter.from_notebook_node(nb)
-    col2.write(st_html(html_output, width=900, height=800, scrolling=True))
+    col2.write(st_html(html_output, height=800, scrolling=True))
 
     ## Creating download button with the updated notebook
     st.sidebar.download_button("Download documented notebook", nb_encoded, "documented_notebook.ipynb", "application/x-ipynb+json")
