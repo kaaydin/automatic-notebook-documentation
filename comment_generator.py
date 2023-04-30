6caedc6bafc5f3b6ec84f105a80b957a58bd9315
kaaydin <kaanaydin95@icloud.com>
2023-05-01 00:09:00 +0200

    Updating files

--- comment_generator.py
+++ comment_generator.py
@@ -4,7 +4,6 @@ API_KEY = "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"
 openai.api_key = API_KEY
 
 MODEL = "gpt-3.5-turbo"
-TEMPERATURE = 0
 
 INSTRUCTION = {"role": "system", "content": """"
         You are a helpful coding assistant that will take as input a cell from a Jupyter notebook and generate 
@@ -12,12 +11,12 @@ INSTRUCTION = {"role": "system", "content": """"
         generated comment to a maximum of 30 words. Ensure that the comments start with a '#' character and provide 
         clear explanations without modifying the code itself. Also, very important, include the code as part of the output. """}
 
-def run_api(message, chosen_model=MODEL, instruction=INSTRUCTION, temperature = TEMPERATURE):
+def run_api(message, chosen_model=MODEL, instruction=INSTRUCTION):
   output = openai.ChatCompletion.create(
     model = chosen_model,
     messages=[instruction,
               message],
-    temperature,
+    temperature = 0,
   )
   
   output_text = output.choices[0]["message"]["content"]
@@ -25,11 +24,11 @@ def run_api(message, chosen_model=MODEL, instruction=INSTRUCTION, temperature = 
 
 def query_message_list(messages):
     
-    new_messages = []
+    documented_messages = []
 
     for message in messages:
       queried_message = {"role": "user", "content": f'{message}'}
-      output = run_api(chosen_model = MODEL, instruction = INSTRUCTION, message = queried_message, temperature=TEMPERATURE)
-      new_messages.append(output)
+      documented_message = run_api(chosen_model = MODEL, instruction = INSTRUCTION, message = queried_message)
+      documented_messages.append(documented_message)
 
-    return new_messages
+    return documented_messages
