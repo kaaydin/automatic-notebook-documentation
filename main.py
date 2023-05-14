0e5c83bfb44c3987406fc57816ca627db5501e1f
kaaydin <kaanaydin95@icloud.com>
2023-05-15 00:02:45 +0200

    Update main

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+## Importing relevant modules
 import argparse
-
 from utils import read_notebook, create_notebook, create_messagelist, save_notebook
 from comment_generator import query_message_list
 
 
 def main(args):
+    """
+    This function processes a notebook file, extracts messages from it, queries the messages,
+    creates a new notebook with the queried messages, and saves the new notebook as a separate file.
+    """
     
-    ##
+    ## Read original notebook
     original_notebook = read_notebook(args.input_file)
     
-    ##
+    ## Create code blocks in list
     original_messages = create_messagelist(original_notebook) 
     
-    ##
+    ## Create documented code blocks
     documented_messages = query_message_list(original_messages)
     
-    ##
+    ## Create new notebook with documented code blocks
     documented_notebook = create_notebook(documented_messages)
     
-    ##
+    ## Save new notebook as a separate file
     save_notebook(documented_notebook, "documented_notebook.ipynb")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate comments for Jupyter notebook cells.")
